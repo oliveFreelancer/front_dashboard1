6a069f97385638795b95d2c411e82d57a7648adb
oliveFreelancer <yellomobile123@naver.com>
2024-12-13 00:02:55 +0900

    메인 레이아웃 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // PrimeReact 테마
+import "primereact/resources/primereact.min.css"; // PrimeReact 기본 스타일
+import "primeicons/primeicons.css"; // PrimeIcons 스타일
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
