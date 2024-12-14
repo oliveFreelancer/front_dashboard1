be3e88304919e8ae67a35b22daaac2a4e2b44b93
oliveFreelancer <yellomobile123@naver.com>
2024-12-15 01:31:20 +0900

    Header 추가

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -1,7 +1,44 @@
 import React from "react";
+import Image from "next/image";
+//components
+import { Menubar } from "primereact/menubar";
 
 const Header = () => {
-  return <div></div>;
+  const items = [
+    {
+      label: "Home",
+      icon: "pi pi-home",
+    },
+    {
+      label: "Features",
+      icon: "pi pi-star",
+    },
+    {
+      label: "Projects",
+      icon: "pi pi-search",
+      items: [
+        {
+          label: "Core",
+          icon: "pi pi-bolt",
+        },
+      ],
+    },
+  ];
+
+  const start = <img alt="logo" src="" height="40" className="mr-2"></img>;
+  const end = (
+    <div className="flex align-items-center gap-2">
+      <Image
+        src="/assets/images/ds.png" // 이미지 파일 경로
+        alt="로고" // 이미지 설명
+        width={500} // 이미지 너비 (픽셀 단위)
+        height={1} // 이미지 높이 (픽셀 단위)
+        layout="responsive"
+      />
+    </div>
+  );
+
+  return <Menubar model={items} start={start} end={end} />;
 };
 
 export default Header;
